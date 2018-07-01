5635dd59101b1c0437ee00f7bfbf243822aeea50
Felipe Ribeiro <phelipperibeiro@hotmail.com>
2018-07-01 20:44:18 -0300

    add ClientService

---
+++ app/Services/ClientService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Service;
+
+class ClientService
+{
+
+    /**
+     *
+     * @var ClientRepository 
+     */
+    private $repository;
+    
+    public function __construct(ClientRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create(Request $request)
+    {        
+        return $this->repository->create($request->all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {       
+        return $this->repository->find($id)->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        return $this->repository->delete($id);
+    }
+
+}
